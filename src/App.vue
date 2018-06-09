33550707bdadab6deeecefd9e1851a38ca91cd40
Mesut Koca <imesutkoca@gmail.com>
2018-06-10 00:36:40 +0300

    build: add eslint

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
 <template>
-  <div id="root">
+  <div class="app">
     <h3>Hello World</h3>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      msg: 'hello world',
+    };
+  },
 
-}
+};
 </script>
 
 <style>
-
+.app{
+font-family: sans-serif;
+}
 </style>
